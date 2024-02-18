6952d58ec3c411f0c7cb35fb814ea03d8941ab50
Tim Beck <socaltdb@gmail.com>
2024-02-18 12:42:46 -0800

    backup script

---
+++ backup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+HOST="root@home.timbeck.me"
+DIRS=(
+    "ftb"
+)
+
+for dir in "${DIRS[@]}"; do
+    source_dir="$HOST:~/$dir"
+    dest_dir="./sync/$dir"
+    mkdir -p "$dest_dir"
+    echo "Sync from $source_dir to $dest_dir"
+    rsync -avz -e ssh "$source_dir" "$dest_dir"
+done
+
+
