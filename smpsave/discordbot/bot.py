e08b81cc41e3f41eee4697d4c4d611c5338a1e26
Tim Beck <becktimd@gmail.com>
2024-02-26 12:20:48 -0800

    move bot methods

--- smpsave/discordbot/bot.py
+++ smpsave/discordbot/bot.py
@@ -1,6 +1,7 @@
 import logging
+import time
 from datetime import datetime, timedelta
-from threading import Lock
+from threading import Event, Lock, Thread
 
 import discord
 from discord.ext import commands
@@ -17,13 +18,86 @@ def discord_intents() -> discord.Intents:
     return intents
 
 
-class BotState():
+class BotBrain():
+    """
+    Handle bot commands given the current state.
+    """
+    config: DiscordBotConfig
+    provisioner: Provisioner
     server_lock: Lock
-    lease_expires: datetime
 
-    def __init__(self):
+    lease_expires: datetime
+    poll_thread: Thread
+    polling_cancel: Event
+
+    def __init__(self, config: DiscordBotConfig, provisioner: Provisioner):
+        self.config = config
+        self.provisioner = provisioner
         self.server_lock = Lock()
         self.lease_expires = datetime.now()
+
+    async def do_start(self, ctx: commands.Context):
+        if self.server_lock.locked():
+            await ctx.send("Start or stop already in progress.")
+            return
+        await ctx.send(f"Starting server, please wait...")
+        with self.server_lock:
+            try:
+                self.provisioner.start()
+                await ctx.send(f"Server started, IP address: {self.provisioner.get_host()}")
+                self.start_lifecycle_polling()
+            except Exception as e:
+                log.error(f"Error starting server: {e}")
+                await ctx.send("Error starting server :(")
+
+    async def do_stop(self, ctx: commands.Context):
+        if self.server_lock.locked():
+            await ctx.send("Start or stop already in progress.")
+            return
+        await ctx.send(f"Stopping server, please wait...")
+        with self.server_lock:
+            try:
+                self.provisioner.stop()
+                self.cancel_lifecycle_polling()
+                await ctx.send(f"Server stopped.")
+            except Exception as e:
+                log.error(f"Error stopping server: {e}")
+                await ctx.send("Error stopping server :(")
+
+    async def do_status(self, ctx: commands.Context):
+        ip = self.provisioner.get_host()
+        if ip:
+            await ctx.send(f"Server is online, ip `{ip}`")
+        elif self.server_lock.locked():
+            # no ip and server is locked, server probably starting.
+            await ctx.send(f"Server is starting.")
+        else:
+            await ctx.send(f"Server is offline.")
+
+    async def do_extend(self, ctx: commands.Context):
+        max_expire_time = \
+            datetime.now() + timedelta(minutes=self.config.lease_max_remaining_minutes)
+        delta = timedelta(minutes=self.config.lease_increment_minutes)
+        target = self.lease_expires + delta
+        if target < max_expire_time:
+            self.lease_expires = target
+        else:
+            self.lease_expires = max_expire_time
+        await ctx.send(f"Lease will now expire at: {self.lease_expires.isoformat()}")
+
+    def start_lifecycle_polling(self):
+        pass
+
+    def cancel_lifecycle_polling(self):
+        pass
+
+    def _shutdown_polling(self):
+        while not self.polling_cancel.is_set():
+            if datetime.now() >= self.lease_expires:
+                log.debug("Lease expired, shutting down server.")
+                self.provisioner.stop()
+                break
+            time.sleep(1)
 
 
 def build_bot(config: DiscordBotConfig, provisioner: Provisioner) -> commands.Bot:
@@ -31,60 +105,29 @@ def build_bot(config: DiscordBotConfig, provisioner: Provisioner) -> commands.Bo
     bot = commands.Bot(command_prefix=config.command_prefix,
                        intents=discord_intents())
 
-    state = BotState()
+    brain = BotBrain(config, provisioner)
 
     @bot.command(help="Provision and start the server")  # type: ignore
     async def start(ctx: commands.Context):
-        if state.server_lock.locked():
-            await ctx.send("Start or stop already in progress.")
-            return
-        await ctx.send(f"Starting server, please wait...")
-        with state.server_lock:
-            try:
-                provisioner.start()
-                await ctx.send(f"Server started, IP address: {provisioner.get_host()}")
-            except Exception as e:
-                log.error(f"Error starting server: {e}")
-                await ctx.send("Error starting server :(")
+        await brain.do_start(ctx)
 
     @bot.command(help="Shut down and deprovision the server")  # type: ignore
     async def stop(ctx: commands.Context):
-        if state.server_lock.locked():
-            await ctx.send("Start or stop already in progress.")
-            return
-        await ctx.send(f"Stopping server, please wait...")
-        with state.server_lock:
-            try:
-                provisioner.stop()
-                await ctx.send(f"Server stopped.")
-            except Exception as e:
-                log.error(f"Error stopping server: {e}")
-                await ctx.send("Error stopping server :(")
+        await brain.do_stop(ctx)
 
     @bot.command(name="extend",  # type: ignore
                  help=f"Extend the lease by {config.lease_increment_minutes} minutes.")
     async def extend_lease(ctx: commands.Context):
-        max_expire_time = datetime.now() + timedelta(minutes=config.lease_max_remaining_minutes)
-        delta = timedelta(minutes=config.lease_increment_minutes)
-        target = state.lease_expires + delta
-        if target < max_expire_time:
-            state.lease_expires = target
-        else:
-            state.lease_expires = max_expire_time
-        await ctx.send(f"Lease will now expire at: {state.lease_expires.isoformat()}")
+        await brain.do_extend(ctx)
 
     @bot.command(name="timeremaining",  # type: ignore
                  help="Get remaining time for the server.")
     async def time_remaining(ctx: commands.Context):
-        await ctx.send(f"Lease will expire at: {state.lease_expires.isoformat()}")
+        await ctx.send(f"Lease will expire at: {brain.lease_expires.isoformat()}")
 
-    @bot.command(name="getip",  # type: ignore
-                 help="Get the IP of the server, if it is online")
+    @bot.command(name="statue",  # type: ignore
+                 help="Get status of the server, and its IP if it is online.")
     async def get_ip(ctx: commands.Context):
-        host = provisioner.get_host()
-        if host:
-            await ctx.send(f"Server online, IP `{host}`")
-        else:
-            await ctx.send("Server offline")
+        await brain.do_status(ctx)
 
     return bot
